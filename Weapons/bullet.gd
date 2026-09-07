f51cd4fa129d098838a9de20e3bca1850cd94dc4
Sean Piche <seanpiche718@gmail.com>
2026-09-07 13:41:37 -0500

    Made bullets despawn over time

--- Weapons/bullet.gd
+++ Weapons/bullet.gd
@@ -5,3 +5,8 @@ var speed = 25
 func _physics_process(delta: float) -> void:
 	var forward = global_transform.basis.z
 	global_position += (forward * speed * delta) / scale.x
+
+
+func _on_timer_timeout() -> void:
+	if is_multiplayer_authority():
+		queue_free()
